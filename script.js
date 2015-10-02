d492b6f43e1c075d79a464bbb1b5e674ef4cdd4f
eternal <tainted.eternity01@gmail.com>
2015-10-02 11:24:17 -0700

    made infinite scroll

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
   var IMDBids = [
     "tt3332064", "tt3488710", "tt3254796", "tt2080374", "tt2118624",
     "tt4226388", "tt1051904", "tt2554274", "tt3682448", "tt3859076",
-    "tt3170832", "tt3272570", "tt1870548", "tt4183692", "tt2473510"
+    "tt3170832", "tt3272570", "tt1870548", "tt4183692", "tt2473510",
+    "tt0111161", "tt0068646", "tt0071562", "tt0468569", "tt0050083",
+    "tt0108052", "tt0060196", "tt0137523", "tt0120737", "tt0080684",
+    "tt0109830", "tt1375666", "tt0073486", "tt0167261", "tt0114369"
   ];
 
   var i = 0;
@@ -11,10 +14,10 @@ $(document).ready(function() {
   function addMovie(i) {
 
     if (i < 10) {
-      var randomNumber = Math.floor( (Math.random() * 10) );
+      var randomNumber = Math.floor( (Math.random() * 30) );
       var randomMovie = IMDBids[randomNumber];
       while ($("#" + randomMovie).length) {
-        var randomNumber = Math.floor( (Math.random() * 10) );
+        var randomNumber = Math.floor( (Math.random() * 30) );
         var randomMovie = IMDBids[randomNumber];
       }
       ajaxCall(randomMovie, i);
@@ -63,5 +66,14 @@ $(document).ready(function() {
   }
 
   addMovie(0);
+
+  $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 10 && i == 0) {
+     if ($(".movie").length < 30) {
+       console.log("scroll call");
+       addMovie(0);
+     }
+   }
+  });
 
 });
